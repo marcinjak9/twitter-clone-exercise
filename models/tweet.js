246191122cf731d646504a18cd12b6b7c5dea035
marcinjak9 <marcinjak9@gmail.com>
2021-05-05 13:01:37 +0200

    fix population like

--- models/tweet.js
+++ models/tweet.js
@@ -17,31 +17,38 @@ const TweetSchema = new Schema({
 
 TweetSchema.statics.like = function (userID, tweetID, callback) {
   this.findOne({ _id: tweetID })
-    .populate('creator')
-    .populate('liked')
     .then((tweet) => {
       if (tweet.liked.includes(userID)) {
         return callback('Tweet already liked')
       }
       tweet.liked.push(userID);
       tweet.like += 1;
-      tweet.save();
-      callback(null, tweet)
+      tweet.save()
+        .then(()=> {
+          this.findOne({ _id: tweetID })
+            .populate('creator')
+            .populate('liked')
+            .then((t) => callback(null, t))
+        })
+      
     }).catch((error) => callback(error, null));
 };
 
 TweetSchema.statics.unlike = function (userID, tweetID, callback) {
   this.findOne({ _id: tweetID })
-    .populate('creator')
-    .populate('liked')
     .then((tweet) => {
       if (!tweet.liked.includes(userID)) {
         return callback('You must like the post first');
       }
       tweet.liked.splice(tweet.liked.indexOf(userID), 1);
       tweet.like -= 1;
-      tweet.save();
-      callback(null, tweet)
+      tweet.save()
+        .then(()=> {
+          this.findOne({ _id: tweetID })
+            .populate('creator')
+            .populate('liked')
+            .then((t) => callback(null, t))
+        })
     })
     .catch((error) => callback(error, null));
 };
